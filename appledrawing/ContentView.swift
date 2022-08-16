18b9f300cfe5ea592ec4fa1be29dc0f4528d3049
shelleyhsieh <shuan0720@gmail.com>
2022-08-16 14:20:25 +0800

    Initial Commit

---
+++ appledrawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  appledrawing
+//
+//  Created by Xie on 2022/8/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ appledrawing/appledrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  appledrawingApp.swift
+//  appledrawing
+//
+//  Created by Xie on 2022/8/16.
+//
+
+import SwiftUI
+
+@main
+struct appledrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
